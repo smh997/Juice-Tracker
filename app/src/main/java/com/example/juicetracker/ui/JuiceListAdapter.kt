5ad22227be081235592e68101f0caa7cd4b28c8b
smh10 <smh10.mh@gmail.com>
2025-01-31 11:15:36 -0500

    A composeView is added to JuiceListAdapter and list_item.xml is removed.

--- app/src/main/java/com/example/juicetracker/ui/JuiceListAdapter.kt
+++ app/src/main/java/com/example/juicetracker/ui/JuiceListAdapter.kt
@@ -15,14 +15,14 @@
  */
 package com.example.juicetracker.ui
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.juicetracker.data.Juice
 import com.example.juicetracker.data.JuiceColor
-import com.example.juicetracker.databinding.ListItemBinding
+
 
 class JuiceListAdapter(
     private var onEdit: (Juice) -> Unit,
@@ -30,37 +30,23 @@ class JuiceListAdapter(
 ) : ListAdapter<Juice, JuiceListAdapter.JuiceListViewHolder>(JuiceDiffCallback()) {
 
     class JuiceListViewHolder(
-        private val binding: ListItemBinding,
+        private val composeView: ComposeView,
         private val onEdit: (Juice) -> Unit,
         private val onDelete: (Juice) -> Unit
-    ) : RecyclerView.ViewHolder(binding.root) {
-        private val nameView = binding.name
-        private val description = binding.description
-        private val drinkImage = binding.drinkColorOverlay
-        private val ratingBar = binding.ratingBar
+    ) : RecyclerView.ViewHolder(composeView) {
 
         fun bind(juice: Juice) {
-            nameView.text = juice.name
-            description.text = juice.description
-            drinkImage.setColorFilter(
-                JuiceColor.valueOf(juice.color).color,
-                android.graphics.PorterDuff.Mode.SRC_IN
-            )
-            ratingBar.rating = juice.rating.toFloat()
-            binding.deleteButton.setOnClickListener {
-                onDelete(juice)
-            }
-            binding.root.setOnClickListener {
-                onEdit(juice)
-            }
+
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = JuiceListViewHolder(
-        ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false),
-        onEdit,
-        onDelete
-    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JuiceListViewHolder {
+        return JuiceListViewHolder(
+            ComposeView(parent.context),
+            onEdit,
+            onDelete
+        )
+    }
 
     override fun onBindViewHolder(holder: JuiceListViewHolder, position: Int) {
         holder.bind(getItem(position))
